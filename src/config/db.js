00ed1e246707e45c147e02ad3f4788641455ca30
Phạm Tuấn Kiệt <167632480+TuanKiet1774@users.noreply.github.com>
2026-08-11 10:09:12 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,36 @@
+// const connectDB = async () => {
+//   const mongoose = require("mongoose");
+//   try {
+//     console.log("⏳ Connecting to MongoDB...");
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       serverSelectionTimeoutMS: 5000,
+//     });
+//     console.log("✅ Connected to MongoDB Atlas");
+//   } catch (err) {
+//     console.error("❌ MongoDB Atlas connection error:", err.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+const mongoose = require("mongoose");
+
 const connectDB = async () => {
-  const mongoose = require("mongoose");
   try {
     console.log("⏳ Connecting to MongoDB...");
+
+    if (mongoose.connection.readyState === 1) {
+      return;
+    }
+
     await mongoose.connect(process.env.MONGO_URI, {
       serverSelectionTimeoutMS: 5000,
     });
+
     console.log("✅ Connected to MongoDB Atlas");
   } catch (err) {
     console.error("❌ MongoDB Atlas connection error:", err.message);
-    process.exit(1);
+    throw err;
   }
 };
 
